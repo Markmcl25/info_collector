de96374cb2233717dbcff1792074d82d3f2441f9
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-27 20:40:03 +0000

    made some changes

--- run.py
+++ run.py
@@ -21,20 +21,21 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_POST(self):
         if self.path == '/submit':
             ctype, pdict = cgi.parse_header(self.headers['content-type'])
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                name = fields.get('name')[0].decode('utf-8')
-                amount = fields.get('amount')[0].decode('utf-8')
-                date = fields.get('date')[0].decode('utf-8')
-                type_ = fields.get('type')[0].decode('utf-8')
-                on_bill = fields.get('on_bill')[0].decode('utf-8')
-                
-                # Append data to Google Sheet
-                sheet.append_row([name, amount, date, type_, on_bill])
-                
-                self.send_response(303)
-                self.send_header('Location', '/')
-                self.end_headers()
+        if ctype == 'multipart/form-data':
+            fields = cgi.parse_multipart(self.rfile, pdict)
+            name = fields.get('name')[0].decode('utf-8')
+            amount = fields.get('amount')[0].decode('utf-8')
+            date = fields.get('date')[0].decode('utf-8')
+            type_ = fields.get('type')[0].decode('utf-8')
+            on_bill = fields.get('on_bill')[0].decode('utf-8')
+            
+            # Append data to Google Sheet
+            client.login()
+            worksheet.append_row([name, amount, date, type_, on_bill])
+            
+            self.send_response(303)
+            self.send_header('Location', '/')
+            self.end_headers()
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
